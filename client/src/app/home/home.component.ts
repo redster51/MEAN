d30815aef1c3e435b3012fad3a522cc2390865f2
redster51 <redster652@gmail.com>
2019-08-20 22:39:35 +0300

    get for all users

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   templateUrl: './home.component.html'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  users = this.getAccounts();
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAccounts(){
+    return this.http.get(`http://localhost:3000/test/users/`);
+  }
+
+  ngOnInit() {
+
+  }
 }

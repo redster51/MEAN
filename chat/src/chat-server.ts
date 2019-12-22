4d0f5024630e0ee27183ad6c9d6ad9d599313be2
redster51 <redster652@gmail.com>
2019-12-22 16:47:36 +0300

    changed angular current version

--- chat/src/chat-server.ts
+++ chat/src/chat-server.ts
@@ -1,3 +1,5 @@
+import SocketIO = require("socket.io");
+
 var mongoose = require('mongoose');
 var dbURI = 'mongodb://redster51:222149id@cluster0-shard-00-00-ykvld.mongodb.net:27017,cluster0-shard-00-01-ykvld.mongodb.net:27017,cluster0-shard-00-02-ykvld.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 
